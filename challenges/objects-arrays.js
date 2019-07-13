4ded8c701fb29d30300e21bd788bfab705ca926b
roselandroche <r.landroche@gmail.com>
2019-07-12 20:53:40 -0400

    Trying to get forEach to work

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -122,9 +122,13 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const animalNames = zooAnimals.forEach(function(element) {
-  return [`${element.animal_name}, ${element.scientific_name}`];
-});;
+// const animalNames = zooAnimals.forEach((zooAnimal) => {
+//   return ['animal_name': zooAnimal.animal_name, 'scientific_name': zooAnimal.scientific_name];
+// }
+
+const animalNames = [zooAnimals.forEach((zooAnimal) => {
+  return `Name: ${zooAnimal.animal_name}, Scientific: ${zooAnimal.scientific_name}.`;
+})];
 console.log(animalNames);
 
 /* Request 2: .map()    
